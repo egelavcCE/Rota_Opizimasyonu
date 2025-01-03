8c82b63b421f3e148aa95147dafde589568cb2e0
Ege Avcı <egelavci2611@gmail.com>
2025-01-04 01:50:49 +0300

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAPye6zAixH9TP3wdMDIG3XHZAGaGBdHeQ",
-  authDomain: "route-9673c.firebaseapp.com",
-  projectId: "route-9673c",
-  storageBucket: "route-9673c.firebasestorage.app",
-  messagingSenderId: "296051896254",
-  appId: "1:296051896254:web:2169804f9a5fa5afa713ef",
-  measurementId: "G-80K6KPYMD3"
+  
 };
 
 // Initialize Firebase
